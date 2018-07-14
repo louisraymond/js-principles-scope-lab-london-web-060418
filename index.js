6e93362991895df9212cd8fb8e28e053c05a331f
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-14 22:08:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const leastFavoriteCustomer = 'some initial value'
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
   
-  //notice that there is no return statement
-  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
 }
 
 function setBestCustomer() {
