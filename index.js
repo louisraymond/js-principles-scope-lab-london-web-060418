62f552146ac0de4a53880259ae2af76bf2a449c3
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-14 21:12:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+
+var customerName="bob";
+
+function upperCaseCustomerName(){ return customerName= customerName.toUpperCase();}
+
+function setBestCustomer(){bestCustomer='not bob';}
+
+function overwriteBestCustomer(){ bestCustomer='maybe bob'; return bestCustomer}
+
+
+const leastFavoeCustomer="dave"
+
+function overwriteBestCustomer(){leastFavoriteCustomer="paul"}
+
+
